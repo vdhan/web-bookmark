3fd504e812377231f321d203a70b80a8db515abe
vdhan <anvudh@gmail.com>
2015-07-19 20:55:26 +0700

    fix miscellaneous

--- js/Anscript.js
+++ js/Anscript.js
@@ -2,7 +2,7 @@ var f2 = true;
 
 function showData()
 {
-	var txt,y,x,i;
+	var txt, y, x, i;
 	var xdoc = loadXML();
 
 	txt = '<table border="1" cellpadding="10" class="Table showData"><tr><th>Tên</th><th>Đường dẫn</th><th>Sửa</th><th>Xóa</th></tr>';
@@ -14,19 +14,19 @@ function showData()
 		y = x[i].getElementsByTagName('name');
 		try {
 			txt = txt + '<td class="col1">' + y[0].firstChild.nodeValue + '</td>';
-		}catch (er) {
+		} catch (er) {
 			txt = txt + '<td class="col1">&nbsp;</td>';
 		}
 
 		y = x[i].getElementsByTagName('url');
 		try {
-			txt=txt + '<td class="col2"><a href="' + y[0].firstChild.nodeValue + '" target="_blank">' + y[0].firstChild.nodeValue + '</a></td>';
-		}catch (er) {
-			txt=txt + '<td class="col2">&nbsp;</td>';
+			txt = txt + '<td class="col2"><a href="' + y[0].firstChild.nodeValue + '" target="_blank">' + y[0].firstChild.nodeValue + '</a></td>';
+		} catch (er) {
+			txt = txt + '<td class="col2">&nbsp;</td>';
 		}
-		txt=txt + '<td><button type="button" class="modBtn" form="mod" title="Sửa đánh dấu này" name="Id" value="' + i + '" >Sửa</button></td>';
-		txt=txt + '<td><button type="submit" class="delBtn" title="Xóa đánh dấu này" form="del" name="id" value="' + i + '">Xóa</button></td>';
-		txt=txt + '</tr>';
+		txt = txt + '<td><button type="button" class="modBtn" form="mod" title="Sửa đánh dấu này" name="Id" value="' + i + '" >Sửa</button></td>';
+		txt = txt + '<td><button type="submit" class="delBtn" title="Xóa đánh dấu này" form="del" name="id" value="' + i + '">Xóa</button></td>';
+		txt = txt + '</tr>';
 	}
 	txt = txt + '</table>';
 	document.getElementById('Data').innerHTML = txt;
@@ -39,13 +39,13 @@ function loadData()
 	var s;
 	var r;
 
-	if(f2 == false)
+	if(!f2)
 	{
 		f2 = true;
 	}
 
 	$('.modBtn').click(function() {
-		if(f2 == true)
+		if(f2)
 		{
 			t = $(this).val();
 			var rowData = $('.rowData');
@@ -84,7 +84,7 @@ $(document).ready(function() {
 			$(this).attr({value: 'Ẩn', title: 'Ẩn đánh dấu'});
 			f1 = 1;
 		}
-		else if(f1==1)
+		else if(f1 == 1)
 		{
 			$('#Data').slideUp('slow');
 			$(this).attr({value: 'Xem', title: 'Xem đánh dấu'});
@@ -98,8 +98,15 @@ $(document).ready(function() {
 		}
 	});
 
-	$('form:first').submit(function() {
-		setTimeout('$("#Clear").click(); showData()', 300);
+	//$('form:first').submit(function() {
+	//	setTimeout('$("#Clear").click(); showData()', 300);
+	//});
+
+	$('#Ok').click(function() {
+		$.post('createData.php', {Name: $('#Name').val(), Url: $('#Url').val()}, function() {
+			$('#Clear').click();
+			showData();
+		});
 	});
 
 	$('#del').submit(function() {
@@ -109,6 +116,6 @@ $(document).ready(function() {
 
 	$('#mod').submit(function() {
 		$('.subBtn').css('display', 'none');
-		setTimeout('showData(); f2=true', 300);
+		setTimeout('showData(); f2 = true', 300);
 	});
 });
--- server/deleteData.php
+++ server/deleteData.php
@@ -1,5 +1,5 @@
 <?php
-$doc = new DOMDocument('1.0','UTF-8');
+$doc = new DOMDocument('1.0', 'UTF-8');
 $doc->load('../bookmark.xml');
 $doc->preserveWhiteSpace = false; 
 $a = $_REQUEST['id'];
